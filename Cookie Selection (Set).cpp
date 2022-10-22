d5e5b1096ac1bbf6e8499f471e8700e7ed4d3220
TayJiunYuan <105603043+TayJiunYuan@users.noreply.github.com>
2022-10-22 17:13:41 +0800

    Update Cookie Selection (Set).cpp

--- Cookie Selection (Set).cpp
+++ Cookie Selection (Set).cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <map>
-#include <vector>
+#include <string>
 #include <algorithm>
 #include <set>
 #include <unordered_map>
@@ -8,33 +8,50 @@
 #include <deque>
 #include <cstdlib>
 
-using namespace std;
+using namespace std; 
 
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    set<int>set;
+    multiset<int>set;
     auto iter = set.begin();
-    for (int i = 0; i < 90; i++){
-        int K; cin >> K;
-        if (set.size() == 0){
-            set.insert(K);
-            iter = set.begin();
-        }
-        else if (set.size() % 2 ==0 ){                   //if set size even
-            set.insert(K);
-            if (K < *iter){                          //set size even and K smaller than median
-                iter -- ; }
-    }
-        else{                                       //if set size odd
-            set.insert(K);
-            if(K > *iter){                         //if set size odd and K bigger than median
+    string M;
+    while (cin >>M){
+        if (M == "#"){
+            if(set.size() % 2 == 0 ){                   //set size even
+                cout<< *iter << endl;
+                auto iter2 = iter;
+                iter -- ;
+                set.erase(iter2);
+            }
+            else{                                      //set size odd
+                cout << *iter << endl;
+                auto iter2 = iter;
                 iter ++;
+                set.erase(iter2);
+            }}
+        else{
+            int K = stoi(M);
+            if (set.size() == 0){
+                set.insert(K);
+                iter = set.begin();
+        }
+            else if (set.size() % 2 ==0 ){                   //if set size even
+                set.insert(K);
+                if (K < *iter){                          //set size even and K smaller than median
+                    iter -- ; }
+    }
+            else{                                       //if set size odd
+                set.insert(K);
+                if(K >= *iter){                         //if set size odd and K bigger than median
+                    iter ++;
             }
         }
-        cout << "median "<< *iter <<endl;
+           // cout << "median "<< *iter <<endl;
         
     }
+}
+    return 0;
 }
