ed678ab736e461db9962afe3f49f98940826dc86
TayJiunYuan <105603043+TayJiunYuan@users.noreply.github.com>
2022-10-23 23:36:04 +0800

    Update RunningMom (Graph DFS Cycle).cpp

--- RunningMom (Graph DFS Cycle).cpp
+++ RunningMom (Graph DFS Cycle).cpp
@@ -8,7 +8,7 @@
 #include <string>
 using namespace std;
 
-unordered_map<string, string>status;                  //initialising status map    unvisited/explored/visited
+unordered_map<string, int>status;                  //initialising status map   0/1/2 =  unvisited/explored/visited
 unordered_map<string, list<string>>AL;                //initialising Adjacency List
 bool cycle_bool = false;
 
@@ -19,17 +19,17 @@ void add_edge(string x, string y){                   //add edge for undirected
 }
 
 void DFS(string V){ //DFS Function                  //changes cycle_bool to true if cycle exists for V
-    status[V] = "explored";
+    status[V] = 1;
     //cout << V;                                                                    //if want to print traversal
     list<string>::iterator i;
     for (i = AL[V].begin(); i != AL[V].end(); ++i){
-        if (status[*i] == "unvisited"){
+        if (status[*i] == 0){
             DFS(*i);}
-        else if (status[*i] == "explored"){
+        else if (status[*i] == 1){
             cycle_bool = true;
         }
     }
-    status[V] = "visited";
+    status[V] = 2;
 }
 
 int main()
@@ -40,8 +40,8 @@ int main()
     for (int i = 0 ; i < N; i++){
         string x; string y; cin >> x >> y;
         add_edge(x, y);                                             //initialising AL
-        status[x]= "unvisited";                                     //Initialising status
-        status[y]= "unvisited";
+        //status[x]= "unvisited";                                     //Initialising status
+        //status[y]= "unvisited";
     }
     string V;
     while (cin >> V){
@@ -50,11 +50,10 @@ int main()
             cout << V << " safe" << endl;
         }
         else{
-            cout << V << " trapped";
+            cout << V << " trapped" << endl;
         }
-        for (auto i: status){
-            status[i.first]= "unvisited";}                        //resetting status
-            cycle_bool = false;                                     //resetting cycle_bool
+        status.clear();                       //resetting status
+        cycle_bool = false;                                     //resetting cycle_bool
         
     }
 }
