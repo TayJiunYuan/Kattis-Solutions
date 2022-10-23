53dfdd20f151d95ad469650ab3eae8c3a0d75003
TayJiunYuan <105603043+TayJiunYuan@users.noreply.github.com>
2022-10-23 23:21:59 +0800

    Update RunningMom (Graph DFS Cycle).cpp

--- RunningMom (Graph DFS Cycle).cpp
+++ RunningMom (Graph DFS Cycle).cpp
@@ -1,34 +1,60 @@
 #include <iostream>
-#include <algorithm>
+#include <map>
 #include <vector>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
 #include <list>
-
+#include <string>
 using namespace std;
+
+unordered_map<string, string>status;                  //initialising status map    unvisited/explored/visited
+unordered_map<string, list<string>>AL;                //initialising Adjacency List
+bool cycle_bool = false;
+
+
+void add_edge(string x, string y){                   //add edge for undirected
+    AL[x].push_back(y);
+    
+}
+
+void DFS(string V){ //DFS Function                  //changes cycle_bool to true if cycle exists for V
+    status[V] = "explored";
+    //cout << V;                                                                    //if want to print traversal
+    list<string>::iterator i;
+    for (i = AL[V].begin(); i != AL[V].end(); ++i){
+        if (status[*i] == "unvisited"){
+            DFS(*i);}
+        else if (status[*i] == "explored"){
+            cycle_bool = true;
+        }
+    }
+    status[V] = "visited";
+}
+
 int main()
 {
-    int N;
-    while (cin >> N , N != -1){
-    int AM[N][N];                           //Initialise AM
-    list<int>checker;
-    for (int i = 0; i<N; i++ ){             //Inputting graph
-        for (int j = 0; j<N; j++){
-            int K; cin >> K;
-            AM[i][j] = K;
-        }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N; cin>>N;
+    for (int i = 0 ; i < N; i++){
+        string x; string y; cin >> x >> y;
+        add_edge(x, y);                                             //initialising AL
+        status[x]= "unvisited";                                     //Initialising status
+        status[y]= "unvisited";
     }
-    for (int i = 0; i<N; i++){
-        bool is_i_weak = true;
-        for (int j = 0; j < N; j++){
-            for (int k = j+1; k<N; k++ ){
-                if (AM[i][j] and AM[i][k] and AM[j][k]){  //check for triangle
-                    is_i_weak = false;
-                }
-            }
+    string V;
+    while (cin >> V){
+        DFS(V);
+        if (cycle_bool){
+            cout << V << " safe" << endl;
         }
-        if (is_i_weak){
-            cout << i << " ";
+        else{
+            cout << V << " trapped";
         }
-    }
-    cout << endl;
+        for (auto i: status){
+            status[i.first]= "unvisited";}                        //resetting status
+            cycle_bool = false;                                     //resetting cycle_bool
+        
     }
 }
