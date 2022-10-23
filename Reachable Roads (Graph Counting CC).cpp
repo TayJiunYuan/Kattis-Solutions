5bf5a6fd01563f56010f668d210d1964906b6f4c
TayJiunYuan <105603043+TayJiunYuan@users.noreply.github.com>
2022-10-23 17:28:57 +0800

    Update Reachable Roads (Graph Counting CC).cpp

--- Reachable Roads (Graph Counting CC).cpp
+++ Reachable Roads (Graph Counting CC).cpp
@@ -1,34 +1,59 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <unordered_map>
 #include <list>
+using namespace std;
+
+unordered_map<int, bool>visited;                  //initialising visited array
+unordered_map<int, list<int>>AL;                  //initialising Adjacency List
+
+
+void add_edge(int x, int y){                   //add edge for undirected
+    AL[x].push_back(y);
+    AL[y].push_back(x);
+}
+
+void DFS(int V){ //DFS Function
+    visited[V] = true;
+    //cout << V;                                                                    //if want to print traversal
+    list<int>::iterator i;
+        for (i = AL[V].begin(); i != AL[V].end(); ++i)
+            if (!visited[*i])
+                DFS(*i);
+}
+
+int counting_CC(int V){
+    int CC = 0;
+    for (int i = 0; i < V; i ++){
+        if (visited[i] == false){
+            CC++;
+            DFS(i);
+        }
+    }
+    return CC;
+}
+
 
 using namespace std;
-int main()
-{
-    int N;
-    while (cin >> N , N != -1){
-    int AM[N][N];                           //Initialise AM
-    list<int>checker;
-    for (int i = 0; i<N; i++ ){             //Inputting graph
-        for (int j = 0; j<N; j++){
-            int K; cin >> K;
-            AM[i][j] = K;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N; cin >> N;
+    for (int i = 0 ; i < N; i++){
+        int V; int E; cin >> V >> E;
+        for (int i = 0; i < V; i++){                //Initiating Visited
+            visited.insert({i,false});
         }
-    }
-    for (int i = 0; i<N; i++){
-        bool is_i_weak = true;
-        for (int j = 0; j < N; j++){
-            for (int k = j+1; k<N; k++ ){
-                if (AM[i][j] and AM[i][k] and AM[j][k]){  //check for triangle
-                    is_i_weak = false;
-                }
-            }
+        for (int i = 0 ; i < E; i++){               //Initialising AL
+            int x; int y; cin >> x >> y;
+            add_edge(x, y);
         }
-        if (is_i_weak){
-            cout << i << " ";
-        }
-    }
-    cout << endl;
+        cout << counting_CC(V)-1 << endl;           //Printout number of CC - 1
+        visited.clear();
+        AL.clear();
     }
 }
+
